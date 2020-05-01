1982bea0d693cdf30db997e15dd6abe221ae7833
Meet Patel <meetcpatel906@gmail.com>
2020-05-01 09:16:30 +0530

    Create index.php

---
+++ RSSTOJSON/index.php
@@ -0,0 +1,9 @@
+extract($_POST);
+
+
+     $fileContents= file_get_contents($url);
+     $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
+     $fileContents = trim(str_replace('"', "'", $fileContents));
+     $simpleXml = simplexml_load_string($fileContents);
+     $json = json_encode($simpleXml);
+     return $json;
